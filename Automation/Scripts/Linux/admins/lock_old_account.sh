550dafe9d57d0c39fbfd09c9c1aae0fc675bb9a5
smortz11 <a.swartz.cs@protonmail.com>
2026-01-30 18:25:55 -0500

    new linux babyy

--- Automation/Scripts/Linux/admins/lock_old_account.sh
+++ Automation/Scripts/Linux/admins/lock_old_account.sh
@@ -11,6 +11,19 @@ fi
 read -p "Enter the admin username to disable: " OLD_ADMIN
 
 if id "$OLD_ADMIN" &>/dev/null; then
+    echo "Enter a new  password for $OLD_ADMIN before disabling:"
+    read -s NEW_PASS
+    echo
+    read -s -p "Confirm password: " CONFIRM_PASS
+    echo
+
+    if [ "$NEW_PASS" != "$CONFIRM_PASS" ]; then
+        echo "Passwords do not match. Exiting."
+        exit 1
+    fi
+
+    echo "$OLD_ADMIN:$NEW_PASS" | chpasswd
+
     echo "Disabling $OLD_ADMIN..."
     usermod -L "$OLD_ADMIN"
     chage -E 0 "$OLD_ADMIN"
