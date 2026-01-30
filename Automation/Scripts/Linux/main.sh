339cb261e0d1fc739f6734b27488b5b6f360b9a2
andrew-ius <144839668+andrew-ius@users.noreply.github.com>
2026-01-29 19:13:41 -0500

    Update main.sh

--- Automation/Scripts/Linux/main.sh
+++ Automation/Scripts/Linux/main.sh
@@ -43,163 +43,60 @@ setup(){
 #! Put all the individual scripts to be run here.
 #############################################################
 run_ubuntu(){
-		#Password setup
-	if grep -q "^$backup1:" /etc/passwd; then
-		#Do not run the password changing scripts
-		echo "You have already changed passwords change the .env backup admins to create new admins and rerun this script."
-	else
-   		backup_admin_setup_ubuntu
-		change_all_user_passwords
-	fi
-
-		#Put external scripts here!
-
-
-
-
-
-
-		#Tools 
-	echo "----Installing Ubuntu tools...----"
-	sudo ./Ubuntu/install_tools_Ubuntu.sh
 	
+	#Install  
+	echo "----Installing Ubuntu tools...----"
+	sudo ./install/install_tools_Ubuntu.sh
+	sudo ./install/banner_change.sh
+	sudo ./install/splunk_Ubuntu.sh
+	
+	#Firewall
 	echo "----Starting UFW config----"
-	sudo ./general/ufw.sh
-	sudo ./general/open_ports_ufw.sh
-
+	sudo ./firewall/ufw.sh
+	sudo ./firewall/open_ports_ufw.sh
+	
+	#Admins
+	echo "----Creating Admin Accounts----"
+	sudo ./admins/create_admins.sh
+	echo "----Locking Old Account----"
+	sudo ./admins/lock_old_account.sh
+	
+	#General
 	echo "----Starting Selinux_config----"
 	sudo ./general/selinux_config.sh
 	
-		#Docker setup for splunk
-	echo "----Setting up Docker Splunk forwarder----"
-	sudo ./general/docker_setup.sh
-
-		#Nmap scan
-	echo "-----Starting Nmap scan-----"
-	sudo ./general/nmap_baseline.sh
-	sudo ./general/nmap_compare.sh
-	
-		# ?Wireshark scan?
-		#Reboot to apply various tools, like selinux, correctly.
+	#Reboot to apply various tools, like selinux, correctly.
 	echo "----Rebooting to apply tools and other settings...----"
 	sleep 8
 	sudo reboot
 }
 
 run_fedora(){
-		#Password setupi
-	if grep -q "^$backup1:" /etc/passwd; then
-		#Do not run the password changing scripts
-		echo "You have already changed passwords change the .env backup admins to create new admins and rerun this script."
-	else
-		backup_admin_setup_fedora
-		change_all_user_passwords
-	fi
-
-		#Put external scripts here!
-	
-		#Tools 
+	#Install
 	echo "-----Installing Fedora tools...-----"
-	sudo ./Fedora/install_tools_Fedora.sh
-		# UFW setup
+	sudo ./install/install_tools_Fedora.sh
+	sudo ./install/banner_change.sh
+	sudo ./install/splunk_Fedora.sh
+	
+	#Firewall
 	echo "-----Starting UFW config------"
-	sudo ./general/ufw.sh
-	sudo ./general/open_ports_ufw.sh
-
-		## Seperate command
+	sudo ./firewall/ufw.sh
+	sudo ./firewall/open_ports_ufw.sh
+	
+	#Admins
+	echo "----Creating Admin Accounts----"
+	sudo ./admins/create_admins.sh
+	echo "----Locking Old Account----"	
+	sudo ./admins/lock_old_account.sh
+	
+	#General
 	echo "----Starting Selinux_config----"
 	sudo ./general/selinux_config.sh
 	
-		#Run set up scripts for each.
-
-		#Docker setup for splunk
-	echo "-----Setting up Docker Splunk forwarder-----"
-	sudo ./general/docker_setup.sh
-
-		#Nmap scan
-	echo "-----Starting Nmap scan-----"
-	sudo ./general/nmap_baseline.sh
-	sudo ./general/nmap_compare.sh
-	
-		#Other stuff
-	echo "----Changing Banner----"
-	sudo ./banner_change.sh
-	
-	
-		# ?Wireshark scan?
+	#Reboot to apply various tools, like selinux, correctly.
 	echo "Rebooting to apply tools and other settings..."
 	sleep 8
 	sudo reboot
-}
-
-backup_admin_setup_ubuntu(){
-	echo "...Setting up"
-	#backup1="batman"
-	#backup2="robin"
-	
-	useradd -m -s /bin/bash $backup1 | echo "+ Created backup user 1"
-	useradd -m -s /bin/bash $backup2 | echo "+ Created backup user 2"
-	usermod -aG sudo $backup1 | echo "+ added 1 to sudoers"
-	usermod -aG sudo $backup2 | echo "+ added 2 to sudoers"
-	#Assign passwords
-	echo "Terminal is on silent mode and won't show you entering the passwords here."
-	read -s -p "Enter password for backup user 1 :" backup1pass
-	echo
-	read -s -p "Enter password for backup user 2 :" backup2pass
-	echo
-	echo "...finishing setting up backup users."
-	echo "$backup1:$backup1pass" | chpasswd
-	echo "$backup2:$backup2pass" | chpasswd
-	#Remove passwords from memory
-	unset backup1pass
-	unset backup2pass
-	#Setup home dir
-	setup_home
-
-	echo "+ Backup admins completed!"
-	echo "Please switch to backup admins, since all other user accounts will be given random passwords."
-}
-
-
-backup_admin_setup_fedora(){
-	echo "...Setting up"
-	#backup1="batman"
-	#backup2="robin"
-	
-	useradd -m -s /bin/bash $backup1 | echo "+ Created backup user 1"
-	useradd -m -s /bin/bash $backup2 | echo "+ Created backup user 2"
-	usermod -aG wheel $backup1 | echo "+ added 1 to sudoers"
-	usermod -aG wheel $backup2 | echo "+ added 2 to sudoers"
-	#Assign passwords
-	echo "Terminal is on silent mode and won't show you entering the passwords here."
-	read -s -p "Enter password for backup user 1 :" backup1pass
-	echo
-	read -s -p "Enter password for backup user 2 :" backup2pass
-	echo
-	echo "...finishing setting up backup users."
-	echo "$backup1:$backup1pass" | chpasswd
-	echo "$backup2:$backup2pass" | chpasswd
-	#Remove passwords from memory
-	unset backup1pass
-	unset backup2pass
-	#setup home dir
-	setup_home
-	echo "+ Backup admins completed!"
-	echo "Please switch to backup admins, since all other user accounts will be given random passwords."
-}
-
-change_all_user_passwords() {
-	for i in $(cat user_list.txt);
-	do
-		PASS=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 31)
-		echo "Changing passwod for $i"
-		
-		#For testing
-		#echo "$PASS" | cat -A
-		
-		#echo "$i,$PASS" >> userlist.txt
-		echo "$i:$PASS" | chpasswd
-	done
 }
 
 setup_home() {
